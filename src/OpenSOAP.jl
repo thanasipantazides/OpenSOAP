4dceff015abf5ebc97c0b9f81869f09822b7daed
Thanasi Pantazides <thanasipantazides@gmail.com>
2025-05-09 17:27:06 -0500

    fixed (temporarily) axis angle derivation from matrix, added DCM interpolation

--- src/OpenSOAP.jl
+++ src/OpenSOAP.jl
@@ -13,11 +13,11 @@ include("visualize.jl")
 
 export LEOSimulation, EarthProperties, Mission, SpacecraftProperties, PowerProperties, DataProperties, MassProperties, SolarPanel, Antenna, AbstractTarget, SunTarget, GroundTarget, MagneticTarget, State, Polyhedron, Maneuver #, FrameFixedTarget
 export load_mission, load_groundstations, load_spacecraft, load_simulation
-export cross, uncross, r_min_arc, r_random, r_euler3, r_euler2, r_euler1, ang321, axis, axisangle, project
+export cross, uncross, r_min_arc, r_random, r_euler3, r_euler2, r_euler1, ang321, axis, axisangle, project, rotinterp
 export can_see_sun, can_see_gnd
 export ECI, ECEF, ICRS, visibility_history, position_eci, position_ecef
 export integrate_system, dynamics_orbit!, dynamics_attitude!, backout_free_torque
-export plot!, polyplot!, plot_earth!, plot_spacecraft!, plot_targets!, plot_detail!, plot_visibilities!, plot_power!, plot_data!, plot_mode!, plot_frames!, plot_meta!, plot_state!, plot_magnetic_field!,  obs_last_orbit_eci, lookat_orbit!, hud_conop!, load_earth_texture_to_ecef
+export plot!, polyplot!, plot_earth!, plot_spacecraft!, plot_targets!, plot_detail!, plot_visibilities!, plot_power!, plot_data!, plot_mode!, plot_frames!, plot_meta!, plot_state!, plot_magnetic_field!, obs_last_orbit_eci, lookat_orbit!, hud_conop!, load_earth_texture_to_ecef
 export mission_stats, write
 
 greet() = print("Hello World!")
--- test/control.jl
+++ test/control.jl
@@ -4,7 +4,7 @@ using InfiniteOpt, Ipopt
 import HSL_jll
 import MathOptInterface as MOI
 using GLMakie, GeometryBasics
-
+import Rotations
 
 # A working example to slew between an initial and target attitude, with zero'd angular rates at both boundaries.
 # Uses direct collocation.
@@ -14,8 +14,9 @@ using GLMakie, GeometryBasics
 # Minimizes RMS control energy cost over the maneuver.
 # Will embed in OpenSOAP and integrate with broader sim.
 function jump_opt_vec()
-    C_BI0 = diagm([1.0,1.0,1.0])
+    C_BI0 = diagm([1.0, 1.0, 1.0])
     C_BIf = r_random()
+    # C_BIf = r_euler3(pi / 4 - 0.1) * r_euler2(pi / 4 - 0.1)
     # w0 = [0.01, 0.02, 0.03]
     w0 = [0.0, 0.0, 0.0]
     wf = [0.0, 0.0, 0.0]
@@ -27,35 +28,36 @@ function jump_opt_vec()
 
     model = Model(Ipopt.Optimizer)
     set_optimizer_attribute(model, "fixed_variable_treatment", "relax_bounds")
-    
+
     set_attribute(model, "hsllib", HSL_jll.libhsl_path)
     set_attribute(model, "linear_solver", "ma57")
+    # set_attribute(model, "linear_solver", "ma86")
     # set_attribute(model, "linear_solver", "ma97")
     # set_attribute(model, "linear_solver", "ma27")
     # set_attribute(model, "ma27_meminc_factor", 4)
     # set_optimizer_attribute(model, "mumps_mem_percent", 4000)
-    
+
     n = 1000
     Tf = 60
-    dt = Tf/n
+    dt = Tf / n
 
-    manstep = Int64(ceil(n - n/4))
+    manstep = Int64(ceil(n - n / 4))
 
     # splat control bounds
     mlimmat = hcat([mlim for i in 1:n]...)
 
     psinf = 1e12
     ident3 = diagm([1.0, 1.0, 1.0])
-    
+
     # splat boundary conditions
-    Climlo = fill(-1.0, (3,3,n))
-    Climhi = fill(1.0, (3,3,n))
+    Climlo = fill(-1.0, (3, 3, n))
+    Climhi = fill(1.0, (3, 3, n))
     wlimmatlo = fill(-psinf, (3, n))
     wlimmathi = fill(psinf, (3, n))
-    Climlo[:,:,1] = C_BI0
-    Climhi[:,:,1] = C_BI0
-    Climlo[:,:,n] = C_BIf
-    Climhi[:,:,n] = C_BIf
+    Climlo[:, :, 1] = C_BI0
+    Climhi[:, :, 1] = C_BI0
+    Climlo[:, :, n] = C_BIf
+    Climhi[:, :, n] = C_BIf
     wlimmatlo[:, 1] = w0
     wlimmathi[:, 1] = w0
     # wlimmatlo[:, n] = wf
@@ -63,18 +65,30 @@ function jump_opt_vec()
     wlimmatlo[:, manstep:n] .= wf
     wlimmathi[:, manstep:n] .= wf
 
+    # find reasonable initial guesses for decision variables
+    # C_guess =
+    # w_guess =
+    # u_guess =
+
+    Cs = rotinterp(C_BI0, C_BIf, length(1:manstep))
+    C_guess = cat(Cs, Climlo[:, :, manstep:n], dims=3)
+
+
     # bounded control:
-    @variable(model, u[i = 1:3, k = 1:n])
+    @variable(model, u[i=1:3, k=1:n])
 
     # angular rates:
     # @variable(model, w_B[i = 1:3, k = 1:n], start = hcat([w0 for i in 1:n]...))
-    @variable(model, w_B[i = 1:3, k = 1:n])
+    @variable(model, w_B[i=1:3, k=1:n])
 
     # attitude:
     @variable(model, C_BI[i=1:3, j=1:3, k=1:n])
+    # Figure out if this syntax is doing what I expect:
+    # @variable(model, C_BI[i=1:3, j=1:3, k=1:n], start = C_guess[i, j, k])
+
 
     # control constraint:
-    @constraint(model, u .<=  mlimmat)
+    @constraint(model, u .<= mlimmat)
     @constraint(model, u .>= -mlimmat)
 
     # angular rate boundary conditions
@@ -88,134 +102,263 @@ function jump_opt_vec()
     angrate_tol = 1e-6
     # SO(3) constraints
     @constraints(model, begin
-        [k=2:n-1], ident3 .- so3_tol .<= C_BI[:,:,k]'*C_BI[:,:,k] .<= ident3 .+ so3_tol
-    end);
+        [k = 2:n-1], ident3 .- so3_tol .<= C_BI[:, :, k]' * C_BI[:, :, k] .<= ident3 .+ so3_tol
+    end)
 
     # explicit determinant for 3x3 matrix
-    op_det_func(X) = X[1,1]*X[2,2]*X[3,3] + X[1,2]*X[2,3]*X[3,1] + X[1,3]*X[2,1]*X[3,2] - X[1,3]*X[2,2]*X[3,1] - X[1,2]*X[2,1]*X[3,3] - X[1,1]*X[2,3]*X[3,2]
+    op_det_func(X) = X[1, 1] * X[2, 2] * X[3, 3] + X[1, 2] * X[2, 3] * X[3, 1] + X[1, 3] * X[2, 1] * X[3, 2] - X[1, 3] * X[2, 2] * X[3, 1] - X[1, 2] * X[2, 1] * X[3, 3] - X[1, 1] * X[2, 3] * X[3, 2]
 
     @operator(model, op_det, 1, op_det_func)
     @constraints(model, begin
-        [k=1:n], 1.0 - so3_tol <= op_det_func(C_BI[:,:,k]) <= 1.0 + so3_tol
-    end);
+        [k = 1:n], 1.0 - so3_tol <= op_det_func(C_BI[:, :, k]) <= 1.0 + so3_tol
+    end)
 
     # dynamic constraints
-    ddt_w(x::Matrix, k::Int) = (x[:,k] .- x[:,k-1]) ./ dt
-    ddt_C(X::Array, w::Array, k::Int) = (X[:,:,k] .- X[:,:,k-1]) ./ dt
+    ddt_w(x::Matrix, k::Int) = (x[:, k] .- x[:, k-1]) ./ dt
+    ddt_C(X::Array, w::Array, k::Int) = (X[:, :, k] .- X[:, :, k-1]) ./ dt
     # ddt_mat(X::Array, w::Array, k::Int) = exp(-(
-    #     [ 0 -w[3,k-1] w[2,k-1]; 
-    #     w[3,k-1] 0 -w[1,k-1]; 
+    #     [ 0 -w[3,k-1] w[2,k-1];
+    #     w[3,k-1] 0 -w[1,k-1];
     #     -w[2,k-1] w[1,k-1] 0]) .* dt)
     # ddt_mat(X::Array, w::Vector, k::Int) = exp(-(
-    #     [ 0 -w[3] w[2]; 
-    #     w[3] 0 -w[1]; 
+    #     [ 0 -w[3] w[2];
+    #     w[3] 0 -w[1];
     #     -w[2] w[1] 0]) .* dt)
 
     @constraint(
         model,
-        [k=2:n], 
-        ddt_w(w_B, k) .<= Jinv*(u[:,k-1] - cross(w_B[:,k-1], J*w_B[:,k-1])) .+ angrate_tol
-    );
+        [k = 2:n],
+        ddt_w(w_B, k) .<= Jinv * (u[:, k-1] - cross(w_B[:, k-1], J * w_B[:, k-1])) .+ angrate_tol
+    )
     @constraint(
         model,
-        [k=2:n], 
-        ddt_w(w_B, k) .>= Jinv*(u[:,k-1] - cross(w_B[:,k-1], J*w_B[:,k-1])) .- angrate_tol
-    );
+        [k = 2:n],
+        ddt_w(w_B, k) .>= Jinv * (u[:, k-1] - cross(w_B[:, k-1], J * w_B[:, k-1])) .- angrate_tol
+    )
     @constraint(
         model,
-        [k=2:n], 
-        ddt_C(C_BI, w_B[:,k], k) .<= -[ 0 -w_B[3,k-1] w_B[2,k-1]; 
-            w_B[3,k-1] 0 -w_B[1,k-1]; 
-            -w_B[2,k-1] w_B[1,k-1] 0]*C_BI[:,:,k-1] .+ so3_tol
-    );
+        [k = 2:n],
+        ddt_C(C_BI, w_B[:, k], k) .<= -[0 -w_B[3, k-1] w_B[2, k-1];
+            w_B[3, k-1] 0 -w_B[1, k-1];
+            -w_B[2, k-1] w_B[1, k-1] 0] * C_BI[:, :, k-1] .+ so3_tol
+    )
     @constraint(
         model,
-        [k=2:n],
-        ddt_C(C_BI, w_B[:,k], k) .>= -[ 0 -w_B[3,k-1] w_B[2,k-1]; 
-        w_B[3,k-1] 0 -w_B[1,k-1]; 
-        -w_B[2,k-1] w_B[1,k-1] 0]*C_BI[:,:,k-1] .- so3_tol 
-    );
+        [k = 2:n],
+        ddt_C(C_BI, w_B[:, k], k) .>= -[0 -w_B[3, k-1] w_B[2, k-1];
+            w_B[3, k-1] 0 -w_B[1, k-1];
+            -w_B[2, k-1] w_B[1, k-1] 0] * C_BI[:, :, k-1] .- so3_tol
+    )
 
-    @objective(model, Min, sum(u[1,:].^2 + u[2,:].^2 + u[3,:].^2))
+    @objective(model, Min, sum(u[1, :] .^ 2 + u[2, :] .^ 2 + u[3, :] .^ 2))
     # @objective(model, Min, sum(
-    #     w_B[1,manstep:n].^2 + 
-    #     w_B[2,manstep:n].^2 + 
+    #     w_B[1,manstep:n].^2 +
+    #     w_B[2,manstep:n].^2 +
     #     w_B[3,manstep:n].^2) + sum(
-    #     u[1,:].^2 + 
-    #     u[2,:].^2 + 
+    #     u[1,:].^2 +
+    #     u[2,:].^2 +
     #     u[3,:].^2
     #     )
     # );
 
     optimize!(model)  # Solve for the control and state
-    
+
     GLMakie.activate!()
     t = 1:n
-    fig = Figure(size=(500,500))
+    fig = Figure(size=(500, 500))
     axu = Axis(
-        fig[2,3],
-        xlabel="i", 
+        fig[2, 3],
+        xlabel="i",
         ylabel="u"
     )
     axw = Axis(
-        fig[3,3],
-        xlabel="i", 
+        fig[3, 3],
+        xlabel="i",
         ylabel="w"
     )
     axc = Axis(
-        fig[1,3],
+        fig[1, 3],
         xlabel="i",
     )
-    
+
     axm = LScene(
-        fig[1:3,1:2]
+        fig[1:3, 1:2]
     )
 
     dethist = zeros(n)
     trhist = zeros(n)
     for i in t
-        dethist[i] = det(value.(C_BI[:,:,i]))
-        trhist[i] = sum((value.(C_BI[:,:,i]))' * value.(C_BI[:,:,i])) .- 2
+        dethist[i] = det(value.(C_BI[:, :, i]))
+        trhist[i] = sum((value.(C_BI[:, :, i]))' * value.(C_BI[:, :, i])) .- 2
     end
 
     set_theme!(theme_light())
-    lines!(axu, t, value.(u[1,:]))
-    lines!(axu, t, value.(u[2,:]))
-    lines!(axu, t, value.(u[3,:]))
-    scatter!(axw, t, value.(w_B[1,:]))
-    scatter!(axw, t, value.(w_B[2,:]))
-    scatter!(axw, t, value.(w_B[3,:]))
+    lines!(axu, t, value.(u[1, :]))
+    lines!(axu, t, value.(u[2, :]))
+    lines!(axu, t, value.(u[3, :]))
+    scatter!(axw, t, value.(w_B[1, :]))
+    scatter!(axw, t, value.(w_B[2, :]))
+    scatter!(axw, t, value.(w_B[3, :]))
     scatter!(axc, t, dethist, label="determinant")
     scatter!(axc, t, trhist, label="orthogonality")
 
-    basis = diagm([1.0,1.0,1.0])
+    basis = diagm([1.0, 1.0, 1.0])
     cols = [:red, :green, :blue]
 
-    println("C_BI Z check max: ", max(value.(C_BI[3,3,:])...))
+    println("C_BI Z check max: ", max(value.(C_BI[3, 3, :])...))
     println("C_BI det min/max: ", min(dethist...), " / ", max(dethist...))
     println("C_BI orth min/max: ", min(trhist...), " / ", max(trhist...))
 
     for b in 1:3
         for i in 1:10:n
-            L = value.(C_BI[:,:,i]*basis[:,b])
+            c = RGBf(0, 0, 0)
+            if b == 1
+                c = RGBAf(1.0, 0.0, 0.0, i / manstep)
+            elseif b == 2
+                c = RGBAf(0.0, 1.0, 0.0, i / manstep)
+            elseif b == 3
+                c = RGBAf(0.0, 0.0, 1.0, i / manstep)
+            end
+            L = value.(C_BI[:, :, i] * basis[:, b])
             lines!(
-                axm, 
+                axm,
                 [0, L[1]],
                 [0, L[2]],
                 [0, L[3]],
-                color=cols[b]
+                color=c
             )
         end
         lines!(
-            axm, 
-            [0, (value.(C_BI[:,:,n])*basis[:,b])[1]],
-            [0, (value.(C_BI[:,:,n])*basis[:,b])[2]],
-            [0, (value.(C_BI[:,:,n])*basis[:,b])[3]],
+            axm,
+            [0, 1.2 * (C_BIf*basis[:, b])[1]],
+            [0, 1.2 * (C_BIf*basis[:, b])[2]],
+            [0, 1.2 * (C_BIf*basis[:, b])[3]],
+            linestyle=:solid,
+            color=cols[b]
+        )
+        lines!(
+            axm,
+            [0, 1.2 * (C_BI0*basis[:, b])[1]],
+            [0, 1.2 * (C_BI0*basis[:, b])[2]],
+            [0, 1.2 * (C_BI0*basis[:, b])[3]],
             linestyle=:dash,
-            color=:black
+            color=cols[b]
         )
     end
 
     axislegend(axc)
-    display(fig) 
+    display(fig)
+end
+
+function test_rotinterp()
+    C_BI0 = diagm([1.0, 1.0, 1.0])
+    # C_BIf = r_random()
+
+    C_BIf = r_euler3(-pi / 3) * r_euler1(-pi + 0.1)
+    (ax, ang) = axisangle(C_BI0' * C_BIf)
+    # println("ax: ", ax)
+    # println("ang: ", ang)
+
+
+    # w0 = [0.01, 0.02, 0.03]
+    w0 = [0.0, 0.0, 0.0]
+    wf = [0.0, 0.0, 0.0]
+
+    J = diagm([0.05, 0.1, 0.15])
+    Jinv = inv(J)
+
+    mlim = [2.0e-3, 2.0e-3, 2.0e-3]
+
+    n = 1000
+    Tf = 60
+    dt = Tf / n
+
+    manstep = Int64(ceil(n - n / 4))
+
+    # splat control bounds
+    mlimmat = hcat([mlim for i in 1:n]...)
+
+    psinf = 1e12
+    ident3 = diagm([1.0, 1.0, 1.0])
+
+    # splat boundary conditions
+    Climlo = fill(-1.0, (3, 3, n))
+    Climhi = fill(1.0, (3, 3, n))
+    wlimmatlo = fill(-psinf, (3, n))
+    wlimmathi = fill(psinf, (3, n))
+    Climlo[:, :, 1] = C_BI0
+    Climhi[:, :, 1] = C_BI0
+    Climlo[:, :, n] = C_BIf
+    Climhi[:, :, n] = C_BIf
+    wlimmatlo[:, 1] = w0
+    wlimmathi[:, 1] = w0
+    # wlimmatlo[:, n] = wf
+    # wlimmathi[:, n] = wf
+    wlimmatlo[:, manstep:n] .= wf
+    wlimmathi[:, manstep:n] .= wf
+
+    # find reasonable initial guesses for decision variables
+    # C_guess =
+    # w_guess =
+    # u_guess =
+
+    Cs = rotinterp(C_BI0, C_BIf, length(1:manstep))
+
+    GLMakie.activate!()
+    t = 1:n
+    fig = Figure(size=(500, 500))
+
+    axm = LScene(
+        fig[1, 1]
+    )
+
+    basis = diagm([1.0, 1.0, 1.0])
+    cols = [:red, :green, :blue]
+
+    for b in 1:3
+        for i in 1:10:manstep
+            c = RGBf(0, 0, 0)
+            if b == 1
+                c = RGBAf(1.0, 0.0, 0.0, i / manstep)
+            elseif b == 2
+                c = RGBAf(0.0, 1.0, 0.0, i / manstep)
+            elseif b == 3
+                c = RGBAf(0.0, 0.0, 1.0, i / manstep)
+            end
+            L = (Cs[:, :, i] * basis[:, b])
+            lines!(
+                axm,
+                [0, L[1]],
+                [0, L[2]],
+                [0, L[3]],
+                color=c
+            )
+        end
+        lines!(
+            axm,
+            [0, 1.2 * (C_BIf*basis[:, b])[1]],
+            [0, 1.2 * (C_BIf*basis[:, b])[2]],
+            [0, 1.2 * (C_BIf*basis[:, b])[3]],
+            linestyle=:solid,
+            color=cols[b]
+        )
+        lines!(
+            axm,
+            [0, 1.2 * (C_BI0*basis[:, b])[1]],
+            [0, 1.2 * (C_BI0*basis[:, b])[2]],
+            [0, 1.2 * (C_BI0*basis[:, b])[3]],
+            linestyle=:dash,
+            color=cols[b]
+        )
+        lines!(
+            axm,
+            [0, 1.2 * ax[1]],
+            [0, 1.2 * ax[2]],
+            [0, 1.2 * ax[3]],
+            linestyle=:solid,
+            color=:black
+        )
+    end
+
+    display(fig)
 end
