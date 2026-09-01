20f64f929d83e54c0847970593fe0beec21d6858
Thanasi Pantazides <thanasipantazides@gmail.com>
2026-08-31 22:59:10 -0500

    organizing the exports in OpenSOAP.jl

--- src/OpenSOAP.jl
+++ src/OpenSOAP.jl
@@ -32,6 +32,7 @@ import Base:
     isvalid,
     iterate,
     hash
+
 import Makie
 
 const SOAP_UNIX_SOCK = "/tmp/soap_out.sock"
@@ -46,23 +47,24 @@ const SOAP_PROTO_TX_VERSION = UInt8(0x00)
 const SOAP_MAX_BUFF_LEN = 2048
 # max number of targets or constraints (each up to this limit) that ModeConfig can depend on.
 const SOAP_MAX_ID_BUCKET = 64
+
+# the longest string we can name an OpenSOAP object
 const SOAP_MAX_STRING_LEN = 255
+
+# type for ID values in this package. These are used to identify structs 
+# that move around on the network in OpenSOAP.
 const IDType = UInt16
+
+# a dictionary that allows lookup of an object by its ID.
 const IDDict = Dict{IDType,T} where {T}
 
 const asset_path = RelocatableFolders.@path joinpath(@__DIR__, "../assets")
-const eop = SatelliteToolboxTransformations.fetch_iers_eop()
 
+# set up SatelliteToolbox
+const eop = SatelliteToolboxTransformations.fetch_iers_eop()
 SatelliteToolboxAtmosphericModels.SpaceIndices.init()
 
 include("types.jl")
-export SOAP_HOST
-export SOAP_MON_PORT
-export SOAP_CORE_PORT
-export SOAP_MAX_STRING_LEN
-export show
-export convert
-export SizedString
 include("util.jl")
 include("core.jl")
 include("visibility.jl")
@@ -73,18 +75,21 @@ include("io.jl")
 include("repl.jl")
 
 
-export worm,
-    cross,
-    uncross,
-    residualSO3,
-    residualso3,
-    dcm_to_quat,
-    r_min_arc,
-    projSO3,
-    IDType,
-    IDDict,
-    IDVector,
-    NetworkMessage,
+
+export SOAP_HOST
+export SOAP_MON_PORT
+export SOAP_CORE_PORT
+export SOAP_REPL_PORT
+export SOAP_MAX_STRING_LEN
+
+# worm
+export worm
+
+# boilerplate types
+export IDType, IDDict, IDVector, SizedString
+
+# objects for the simulation
+export NetworkMessage,
     AbstractState,
     AbstractConfig,
     AbstractTarget,
@@ -102,32 +107,29 @@ export worm,
     SatelliteConfig,
     ModeConfig,
     TargetConfig,
-    SimConfig,
-    find_config,
-    ser,
-    des,
-    packetize,
-    unpacketize,
-    behead,
-    setup_server,
-    setup_client,
-    read_transport,
-    write_transport,
-    step_orbit,
-    step_satellite,
-    step_earth,
-    step!,
-    run,
-    run_free,
-    monitor,
-    write_csv,
-    load_earth_texture_to_ecef,
-    load_jsonc,
-    load_config,
-    check_ids,
-    update
+    SimConfig
+export find_config
 
-export reinterpret
+# serialization/deserialization and network methods
+export ser, des, packetize, unpacketize, behead
+export setup_server, setup_client, read_transport, write_transport
+
+# attitude utilities
+export cross, uncross, residualSO3, residualso3, dcm_to_quat, r_min_arc, projSO3
+
+# simulation stepping methods
+export step_orbit, step_satellite, step!
+
+# core and visualizer
+export run, run_free, monitor
+
+# io and loading
+export write_csv, load_earth_texture_to_ecef, load_jsonc, load_config, check_ids
+
+# REPL interface
+export update
+
+export reinterpret, show, convert
 
 BUILD = false
 if BUILD
